520bc6d7ce0babb46f2c77428dc8e4f9747489fc
sumin0407 <smpark@se.kaist.ac.kr>
2020-08-12 20:59:47 +0900

    Create RuntimeVerification class on verifier package. Not sure to use it or not

---
+++ engine/src/verifier/RuntimeVerification.java
@@ -0,0 +1,16 @@
+package verifier;
+
+import property.PropertyChecker;
+
+
+
+public class RuntimeVerification extends Verifier {
+
+    public RuntimeVerification(PropertyChecker checker) {
+        super(checker);
+    }
+
+    public boolean checker() {
+        //verification result?
+    }
+}
