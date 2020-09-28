5983a50dddc9ac308d97c9d97b32474c7bbd381b
Rienshaliu <rienshaliu@gmail.com>
2020-09-28 11:43:20 +0900

    add prevention property checker

---
+++ engine/src/property/MCIPreventionChecker.java
@@ -0,0 +1,41 @@
+package property;
+
+import log.Log;
+import log.Snapshot;
+import property.pattern.PreventionChecker;
+
+public class MCIPreventionChecker extends PreventionChecker {
+
+    public MCIPreventionChecker() {
+
+        super();
+    }
+
+    @Override
+    protected boolean evaluateState(Log log, Property verificationProperty) {
+        return false;
+    }
+
+    @Override
+    public boolean check(Log log, Property verificationProperty, int until) {
+        return false;
+    }
+
+    @Override
+    public boolean check(Log log, Property verificationProperty, double prob, int T) {
+        return false;
+    }
+
+    @Override
+    public boolean check(Log log, Property verificationProperty, double prob, int t, int T) {
+        return false;
+    }
+
+    @Override
+    public boolean check(Log log, Property verificationProperty, int t, int T) {
+        return false;
+    }
+
+    @Override
+    protected boolean evaluateState(Snapshot snapshot, Property verificationProperty) {return false; }
+}
