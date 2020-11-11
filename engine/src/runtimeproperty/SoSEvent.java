01b5dba70ffc934e5215130e94d3845503c1f4d9
proseuche <proseuche@hanmail.net>
2020-11-11 13:35:52 +0900

    SoSEvent name

--- engine/src/runtimeproperty/SoSEvent.java
+++ engine/src/runtimeproperty/SoSEvent.java
@@ -3,5 +3,8 @@ package runtimeproperty;
 import log.Snapshot;
 
 public abstract class SoSEvent extends Event {
+    public SoSEvent(String name){
+        super(name);
+    }
     public abstract boolean checkHold(Snapshot snapshot);
 }
